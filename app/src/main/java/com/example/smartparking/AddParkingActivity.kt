191b63b15772c163b0b87eb88d59818f6e86b511
EGuzzini <guzzini.emanuele21@gmail.com>
2019-12-04 17:58:50 +0100

    added permission for network usage in manifest

--- app/src/main/java/com/example/smartparking/AddParkingActivity.kt
+++ app/src/main/java/com/example/smartparking/AddParkingActivity.kt
@@ -8,5 +8,6 @@ class AddParkingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_parking)
+
     }
 }
