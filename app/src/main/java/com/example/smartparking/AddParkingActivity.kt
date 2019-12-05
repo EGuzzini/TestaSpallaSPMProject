dcb0fd445dde6ee6aa1ecd8f952876c11a30fc2c
EGuzzini <guzzini.emanuele21@gmail.com>
2019-12-05 12:28:20 +0100

    added listeners to buttons and retreived data from textboxes

--- app/src/main/java/com/example/smartparking/AddParkingActivity.kt
+++ app/src/main/java/com/example/smartparking/AddParkingActivity.kt
@@ -1,13 +1,26 @@
 package com.example.smartparking
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_parking.*
+import android.widget.TextView
+
+
 
 class AddParkingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_parking)
+        add_button.setOnClickListener {
+            var position_text = position_textbox.text.toString()
+            var municipality_text = municipality_textbox.text.toString()
+            var price_text = price_textbox.text.toString()
+            val textView = findViewById<TextView>(R.id.text)
 
+
+
+            }
+         }
     }
-}
