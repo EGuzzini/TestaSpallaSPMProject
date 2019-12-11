f701206fc93e342a51c2f1e58a5e0aa6a25b36b8
William Taruschio <william.taruschio@studenti.unicam.it>
2019-12-11 14:56:43 +0100

    driver API crud

--- app/routes/appRoutes.js
+++ app/routes/appRoutes.js
@@ -3,22 +3,33 @@ const express = require('express');
 const router = express.Router();
 
 const parkingslot = require("../controller/parkingController.js");
-
+const driver = require("../controller/driverController.js");
 // Create a new Parking slot
 router.post("/parking", parkingslot.create);
 // Retrieve all Parking slots
 router.get("/parking", parkingslot.findAll);
-
 // Retrieve a single Parking slot with parkingId
 router.get("/parking/:parkingId", parkingslot.findOne);
-
-// Update a Customer with customerId
+// Update a Parking slot with parkingId
 router.put("/parking/:parkingId", parkingslot.update);
-
-// Delete a Customer with customerId
+// Delete a Parking slot with parkingId
 router.delete("/parking/:parkingId", parkingslot.delete);
-
-// Create a new Customer
+// delete all parking slots
 router.delete("/parking", parkingslot.deleteAll);
+
+
+// routes for driver
+//create a new user
+router.post("/register", driver.create);
+//get all users
+router.get("/users", driver.findAll);
+// Retrieve a single driver with parkingId
+router.get("/users/:driverId", driver.findOne);
+// Update a driver with driverId
+router.put("/users/:driverId", driver.update);
+// Delete a Parking slot with parkingId
+router.delete("/users/:driverId", driver.delete);
+// delete all parking slots
+router.delete("/users", driver.deleteAll);
 
 module.exports = router;
