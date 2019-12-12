1694cecaf9df95eca030dce681a1f139ac45d242
Emanuele <Guzzini>
2019-12-12 16:41:37 +0100

    fake commit

--- app/src/main/java/com/example/smartparking/ParkingActivity.kt
+++ app/src/main/java/com/example/smartparking/ParkingActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_parking.*
 
 class ParkingActivity : AppCompatActivity() {
-
+    //iiiiii
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parking)
