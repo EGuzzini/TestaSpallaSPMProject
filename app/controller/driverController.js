5587cdaef21bd75e087b790b2147bbdc05e65341
Dante <dante.domizi@studenti.unicam.it>
2019-12-12 23:05:08 +0100

    Add tests for drivers

--- app/controller/driverController.js
+++ app/controller/driverController.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const Driver = require("../models/driverModel.js");
 
-let passwordhash="";
+let passwordhash = "";
 exports.create = (req, res) => {
     // Validate request
     if (!req.body) {
@@ -10,11 +10,11 @@ exports.create = (req, res) => {
             message: "Content can not be empty!"
         });
     }
-    bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
         if (err) {
             throw err
         } else {
-            bcrypt.hash(req.body.password, salt, function (err, hash) {
+            bcrypt.hash(req.body.password, salt, function(err, hash) {
                 if (err) {
                     throw err
                 } else {
@@ -23,7 +23,7 @@ exports.create = (req, res) => {
                         email: req.body.email,
                         passwordhash: hash
                     });
-                    Driver.findByEmailOrUsername(d.email,d.username, (err, data) => {
+                    Driver.findByEmailOrUsername(d.email, d.username, (err, data) => {
                         console.log(data);
                         if (data !== null) {
                             res.status(404).send({
@@ -34,10 +34,9 @@ exports.create = (req, res) => {
                             Driver.create(d, (err, data) => {
                                 if (err)
                                     res.status(500).send({
-                                        message:
-                                            err.message || "Some error occurred while creating the Customer."
+                                        message: err.message || "Some error occurred while creating the Customer."
                                     });
-                                else res.send().status(200);
+                                else res.send(data).status(200);
                             });
                         }
                     });
@@ -46,94 +45,92 @@ exports.create = (req, res) => {
         }
     })
 
- 
+
 
 
 };
 exports.findAll = (req, res) => {
     Driver.getAll((err, data) => {
-      if (err)
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving drivers."
-        });
-      else res.send(data);
+        if (err)
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving drivers."
+            });
+        else res.send(data);
     });
-  };
+};
 
-  exports.findOne = (req, res) => {
+exports.findOne = (req, res) => {
     Driver.findById(req.params.driverId, (err, data) => {
-  
-      console.log(req.params.driverId + " driver id");
-      if (err) {
-        if (err.kind === "not_found") {
-          res.status(404).send({
-            message: `Not found driver with id ${req.params.driverId}.`
-          });
-        } else {
-          res.status(500).send({
-            message: "Error retrieving driver with id " + req.params.driverId
-          });
-        }
-      } else res.send(data);
-    });
-  };
 
-  exports.update = (req, res) => {
+        console.log(req.params.driverId + " driver id");
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found driver with id ${req.params.driverId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving driver with id " + req.params.driverId
+                });
+            }
+        } else res.send(data);
+    });
+};
+
+exports.update = (req, res) => {
     // Validate Request
     if (!req.body) {
-      res.status(400).send({
-        message: "Content can not be empty!"
-      });
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
     }
     console.log(req.params.driverId);
-    
-  
+
+
     Driver.updateById(
-  
-  
-      req.params.driverId,
-      new Driver(req.body),
-      (err, data) => {
-        
-        if (err) {
-          if (err.kind === "not_found") {
-            res.status(404).send({
-              message: `Not found Driver with id ${req.params.driverId}.`
-            });
-          } else {
-            res.status(500).send({
-              message: "Error updating Driver with id " + req.params.driverId
-            });
-          }
-        } else res.send(data);
-      }
+
+
+        req.params.driverId,
+        new Driver(req.body),
+        (err, data) => {
+
+            if (err) {
+                if (err.kind === "not_found") {
+                    res.status(404).send({
+                        message: `Not found Driver with id ${req.params.driverId}.`
+                    });
+                } else {
+                    res.status(500).send({
+                        message: "Error updating Driver with id " + req.params.driverId
+                    });
+                }
+            } else res.send(data);
+        }
     );
-  };
-  
+};
+
 exports.delete = (req, res) => {
     Driver.remove(req.params.driverId, (err, data) => {
-      if (err) {
-        if (err.kind === "not_found") {
-          res.status(404).send({
-            message: `Not found Parking slot with id ${req.params.driverId}.`
-          });
-        } else {
-          res.status(500).send({
-            message: "Could not delete driver with id " + req.params.driverId
-          });
-        }
-      } else res.send({ message: `driver was deleted successfully!` });
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found Parking slot with id ${req.params.driverId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Could not delete driver with id " + req.params.driverId
+                });
+            }
+        } else res.send({ message: `driver was deleted successfully!` });
     });
-  };
+};
 
-  exports.deleteAll = (req, res) => {
+exports.deleteAll = (req, res) => {
     Driver.removeAll((err, data) => {
-      if (err)
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while removing all drivers."
-        });
-      else res.send({ message: `All drivers were deleted successfully!` });
+        if (err)
+            res.status(500).send({
+                message: err.message || "Some error occurred while removing all drivers."
+            });
+        else res.send({ message: `All drivers were deleted successfully!` });
     });
-  };
+};
--- test/testParking.js
+++ test/testParking.js
@@ -3,7 +3,8 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { expect } = chai;
 chai.use(chaiHttp);
-let pID = 0;
+let parkingId = 0;
+
 describe("Server!", () => {
     it("welcomes user to the api", (done) => {
         chai
@@ -44,7 +45,7 @@ describe("Server!", () => {
 
 
         });
-        it('it should POST a parking with the same coordinates  ', (done) => {
+        it('try to POST a parking with the same coordinates  ', (done) => {
             let parking = {
                 status: 0,
                 posx: 1,
@@ -57,18 +58,19 @@ describe("Server!", () => {
                 .send(parking)
                 .end((err, res) => {
                     expect(res).to.have.status(404);
+                    expect(res.body.message).to.equals("already exist a Parking with posx " + parking.posx + " and posy " + parking.posy + " .");
                     done();
                 });
         });
     });
 
-    describe('/GET/ALL', () => {
+    describe('/GET/ALL PARKING', () => {
         it("it should GET all parking", done => {
             chai.request(app)
                 .get('/parking/')
                 .end((err, res) => {
                     expect(res).to.have.status(200);
-                    pID = res.body[0].idparkingslot;
+                    parkingId = res.body[0].idparkingslot;
                     done();
                 });
 
@@ -77,10 +79,22 @@ describe("Server!", () => {
     describe('/GET/:id', () => {
         it("it should GET a parking given the id'", done => {
             chai.request(app)
-                .get('/parking/' + pID)
+                .get('/parking/' + parkingId)
                 .end((err, res) => {
                     expect(res).to.have.status(200);
 
+                    done();
+                });
+
+        });
+        it("try to GET a parking given the wrong id'", done => {
+            let id = 0;
+            chai.request(app)
+
+            .get('/parking/' + id)
+                .end((err, res) => {
+                    expect(res).to.have.status(404);
+                    expect(res.body.message).to.equals("Not found Parking with id " + id + ".");
                     done();
                 });
 
@@ -97,35 +111,78 @@ describe("Server!", () => {
             }
 
             chai.request(app)
-                .put('/parking/' + pID)
+                .put('/parking/' + parkingId)
                 .send(parking)
                 .end((err, res) => {
                     expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('status', 0);
+                    expect(res.body).to.have.property('posx', 3.2);
+                    expect(res.body).to.have.property('posy', 5.5);
+                    expect(res.body).to.have.property('comune', "Roma");
+                    expect(res.body).to.have.property('costoorario', 6.3);
                     done();
                 });
 
         });
+        it("try to UPDATE a parking given the wrong id'", done => {
+            let parking = {
+                status: 0,
+                posx: 3.2,
+                posy: 5.5,
+                comune: "Roma",
+                costoorario: 6.3
+            }
+            let id = 0;
+            chai.request(app)
+                .put('/parking/' + id)
+                .send(parking)
+                .end((err, res) => {
+                    expect(res).to.have.status(404);
+                    expect(res.body.message).to.equals("Not found Parking slot with id " + id + ".");
+                    done();
+                });
+
+        });
+
+
     });
     describe('/DELETE/:id', () => {
         it('it should DELETE a parking given the id', (done) => {
+            let id = 0;
 
             chai.request(app)
-                .delete('/parking/' + pID)
+                .delete('/parking/' + id)
+                .end((err, res) => {
+                    expect(res).to.have.status(404);
+                    expect(res.body.message).to.equals("Not found Parking slot with id " + id + ".");
+                    done();
+                });
+        });
+        it('try to DELETE a parking given the wrong id', (done) => {
+
+            chai.request(app)
+                .delete('/parking/' + parkingId)
                 .end((err, res) => {
                     expect(res).to.have.status(200);
+                    expect(res.body.message).to.equals("Parking slot was deleted successfully!");
+                    done();
+                });
+        });
+
+    });
+
+    describe('/DELETE/ALL', () => {
+        it('it should DELETE all parking', (done) => {
+            chai.request(app)
+                .delete('/parking')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body.message).to.equals("All Parking slot were deleted successfully!");
+
                     done();
                 });
         });
     });
 
-    /*  describe('/DELETE/ALL', () => {
-          it('it should DELETE a parking given the id', (done) => {
-              chai.request(app)
-                  .delete('/parking')
-                  .end((err, res) => {
-                      expect(res).to.have.status(200);
-                      done();
-                  });
-          });
-      });*/
+
 });
