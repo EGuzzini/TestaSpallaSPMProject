9cbfcfd21f3b11cd290d412ba68edb79fa49bac7
Emanuele <Guzzini>
2019-12-18 12:32:23 +0100

    implemented jwt and sharedfiles

--- FrontEnd/app/src/main/java/com/example/smartparking/LoginActivity.kt
+++ FrontEnd/app/src/main/java/com/example/smartparking/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.smartparking
 
+import android.R.attr.name
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.os.StrictMode
+import android.preference.PreferenceManager
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,11 +15,12 @@ import java.io.*
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
-import java.util.HashMap
+import java.util.*
 
 
 class LoginActivity : AppCompatActivity() {
-
+    val PREFS_FILENAME = "com.example.smartparking.prefs"
+    var prefs :SharedPreferences? = null
     private var doubleBackToExitPressedOnce = false
     override fun onBackPressed() {
         if (doubleBackToExitPressedOnce) {
@@ -30,6 +34,9 @@ class LoginActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        var tokenget=""
+        prefs = this.getSharedPreferences(PREFS_FILENAME, 0)
+    var token=""
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         login_button.setOnClickListener {
@@ -40,64 +47,79 @@ class LoginActivity : AppCompatActivity() {
                 var username = username_login.text.toString()
                 var password = password_login.text.toString()
 
-                    val params: HashMap<String, String> =
-                        object : HashMap<String, String>() {
-                            init {
-                                put("email", "sam")
-                                put("username", username)
-                                put("password", password)
-                            }
+                val params: HashMap<String, String> =
+                    object : HashMap<String, String>() {
+                        init {
+                            put("email", "sam")
+                            put("username", username)
+                            put("password", password)
                         }
-                    val sbParams = java.lang.StringBuilder()
-                    var i = 0
-                    for (key in params.keys) {
-                        try {
-                            if (i != 0) {
-                                sbParams.append("&")
-                            }
-                            sbParams.append(key).append("=")
-                                .append(URLEncoder.encode(params[key], "UTF-8"))
-                        } catch (e: UnsupportedEncodingException) {
-                            e.printStackTrace()
-                        }
-                        i++
                     }
+                val sbParams = java.lang.StringBuilder()
+                var i = 0
+                for (key in params.keys) {
                     try {
-                        val url = getString(R.string.connection) + "login"
-                        val urlObj = URL(url)
-                        val conn = urlObj.openConnection() as HttpURLConnection
-                        conn.doOutput = true
-                        conn.requestMethod = "POST"
-                        conn.setRequestProperty("Accept-Charset", "UTF-8")
-                        conn.readTimeout = 1500
-                        conn.connectTimeout = 3000
-                        conn.connect()
-                        val paramsString: String = sbParams.toString()
-                        val wr = DataOutputStream(conn.outputStream)
-                        wr.writeBytes(paramsString)
-                        wr.flush()
-                        wr.close()
-                        try {
-                            val `in`: InputStream = BufferedInputStream(conn.inputStream)
-                            val reader = BufferedReader(InputStreamReader(`in`))
-                            val result = StringBuilder()
-                            var line: String?
-                            while (reader.readLine().also { line = it } != null) {
-                                result.append(line)
-                            }
-                            Log.d("test", "result from server: $result")
-                        } catch (e: IOException) {
-                            e.printStackTrace()
-                        } finally {
-                            conn.disconnect()
+                        if (i != 0) {
+                            sbParams.append("&")
                         }
-                    } catch (e: IOException) {
+                        sbParams.append(key).append("=")
+                            .append(URLEncoder.encode(params[key], "UTF-8"))
+                    } catch (e: UnsupportedEncodingException) {
                         e.printStackTrace()
                     }
+                    i++
+                }
+
+
+                try {
+                    val url = getString(R.string.connection) + "login"
+                    val urlObj = URL(url)
+                    val conn = urlObj.openConnection() as HttpURLConnection
+                    conn.doOutput = true
+                    conn.requestMethod = "POST"
+                    conn.setRequestProperty("Accept-Charset", "UTF-8")
+                    conn.readTimeout = 1500
+                    conn.connectTimeout = 3000
+                    conn.connect()
+                    val paramsString: String = sbParams.toString()
+                    val wr = DataOutputStream(conn.outputStream)
+                    wr.writeBytes(paramsString)
+                    wr.flush()
+                    wr.close()
+                    try {
+                        val `in`: InputStream = BufferedInputStream(conn.inputStream)
+                        val reader = BufferedReader(InputStreamReader(`in`))
+                        val result = StringBuilder()
+                        val tokenresult = StringBuilder()
+                        var line: String?
+                        while (reader.readLine().also { line = it } != null) {
+                            result.append(line)
+
+                        }
+                        var i =13
+                        do{
+                           tokenresult.append(result.get(i))
+                            i++
+                        }while(result.get(i)!='"')
+                          token=tokenresult.toString()
+                       val editor: SharedPreferences.Editor = prefs!!.edit()
+                        editor.putString("token",token)
+                        editor.apply()
+                       Log.d("test token",prefs!!.getString("token",tokenget))
+                        Log.d("test", "result from server: $result")
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } finally {
+                        conn.disconnect()
+                        val mapact = Intent(this, ParkingActivity::class.java)
+                        startActivity(mapact)
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
 
             }
-            //val mapact = Intent(this, MapActivity::class.java)
-           //startActivity(mapact)
+
         }
         RegisterHereText.setOnClickListener {
             val regact = Intent(this, RegisterActivity::class.java)
--- FrontEnd/app/src/main/java/com/example/smartparking/ParkingActivity.kt
+++ FrontEnd/app/src/main/java/com/example/smartparking/ParkingActivity.kt
@@ -1,19 +1,26 @@
 package com.example.smartparking
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_parking.*
 
 class ParkingActivity : AppCompatActivity() {
-
+    var prefs : SharedPreferences? = null
+    val PREFS_FILENAME = "com.example.smartparking.prefs"
     override fun onCreate(savedInstanceState: Bundle?) {
+        prefs = this.getSharedPreferences(PREFS_FILENAME, 0)
+        var tokenget=""
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parking)
         fab.setImageResource(R.drawable.ic_action_add)
+        Log.d("porcoddio",prefs!!.getString("token",tokenget))
         fab.setOnClickListener {
             val addpark = Intent(this, AddParkingActivity::class.java)
             startActivity(addpark)
+
         }
     }
 
