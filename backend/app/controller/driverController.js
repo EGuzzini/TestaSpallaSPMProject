b857128ad059eeec15867c62815bdd6cfecdedc5
William Taruschio <william.taruschio@studenti.unicam.it>
2020-01-17 10:50:12 +0100

    nearest done

--- backend/app/controller/driverController.js
+++ backend/app/controller/driverController.js
@@ -5,6 +5,10 @@ const jwt = require("jsonwebtoken");
 const config = require("../../config.js")
 const nodemailer = require('nodemailer');
 const randomstring = require("randomstring");
+
+exports.checkTokenInit = (req, res) => {
+    res.send().status(200)
+}
 exports.create = (req, res) => {
     console.log("req body :  " + req.body.username);
     // Validate request
@@ -13,11 +17,11 @@ exports.create = (req, res) => {
             message: "Content can not be empty!"
         });
     }
-    bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
         if (err) {
             throw err
         } else {
-            bcrypt.hash(req.body.password, salt, function(err, hash) {
+            bcrypt.hash(req.body.password, salt, function (err, hash) {
                 if (err) {
                     throw err
                 } else {
@@ -58,9 +62,8 @@ exports.login = (req, res) => {
         });
     }
     let getDriver;
-
-    Driver.findByEmailOrUsername(req.body.email, req.body.username, (err, driver) => {
-        console.log(req.body.username + "   username ");
+    Driver.findByEmailOrUsername("", req.body.username, (err, driver) => {
+        console.log(JSON.stringify(driver) + "   username ");
         if (!driver) {
             return res.status(401).json({
                 message: "Authentication failed"
@@ -69,51 +72,24 @@ exports.login = (req, res) => {
         getDriver = driver;
         return bcrypt.compare(req.body.password, driver.password, (err, response) => {
             if (!response) {
-                return res.status(401).json({
-                    message: "Authentication failed"
-                });
+                return res.status(401).json({ error: "Authentication failed" }
+                );
             }
             console.log(response);
             let jwtToken = jwt.sign({
-                email: getDriver.email,
-                driverId: getDriver.driverId
+                email: driver.email,
+                driverId: driver.idDriver,
+                admin: false
             }, config.secret, {
-                expiresIn: "1h"
+                expiresIn: "20m"
             });
             res.status(200).json({
                 jwtToken
             });
         })
     });
-  }
-  let getDriver;
-  Driver.findByEmailOrUsername("",req.body.username, (err, driver) => {
-    console.log(JSON.stringify(driver)+ "   username ");
-    if (!driver) {
-      return res.status(401).json({
-        message: "Authentication failed"
-      });
-    }
-    getDriver = driver;
-    return bcrypt.compare(req.body.password, driver.password, (err, response) => {
-      if (!response) {
-        return res.status(401).json({ error: "Authentication failed" }
-        );
-      }
-      console.log(response);
-      let jwtToken = jwt.sign({
-        email: driver.email,
-        driverId: driver.idDriver,
-        admin: false
-      }, config.secret, {
-        expiresIn: "20m"
-      });
-      res.status(200).json({
-        jwtToken
-      });
-    })
-  });
 }
+
 
 exports.findAll = (req, res) => {
     Driver.getAll((err, data) => {
@@ -177,25 +153,25 @@ exports.update = (req, res) => {
 
 //cancella l'account del driver prendendo il driverId dal token 
 exports.delete = (req, res) => {
-  console.log(req.decoded.driverId+" id driver token")
-  /* prende il campo admin dal token per vedere se ha i diritti di accesso per creare i parcheggi
-  if(req.decoded.admin==false){
-    return res.status(401);
-  }
-  */
-  Driver.remove(req.decoded.driverId, (err, data) => {
-    if (err) {
-      if (err.kind === "not_found") {
-        res.status(404).send({
-          message: `Not found Parking slot with id ${req.decoded.driverId}.`
-        });
-      } else {
-        res.status(500).send({
-          message: "Could not delete driver with id " + req.decoded.driverId
-        });
-      }
-    } else res.send({ message: `driver was deleted successfully!` });
-  });
+    console.log(req.decoded.driverId + " id driver token")
+    /* prende il campo admin dal token per vedere se ha i diritti di accesso per creare i parcheggi
+    if(req.decoded.admin==false){
+      return res.status(401);
+    }
+    */
+    Driver.remove(req.decoded.driverId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found Parking slot with id ${req.decoded.driverId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Could not delete driver with id " + req.decoded.driverId
+                });
+            }
+        } else res.send({ message: `driver was deleted successfully!` });
+    });
 };
 
 exports.deleteAll = (req, res) => {
@@ -223,7 +199,7 @@ exports.changePassword = (req, res) => {
                 });
             }
         } else {
-            bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.genSalt(saltRounds, function (err, salt) {
                 if (err) {
                     throw err
                 } else {
@@ -234,11 +210,11 @@ exports.changePassword = (req, res) => {
                                 message: "The old password is wrong"
                             });
                         } else {
-                            bcrypt.genSalt(saltRounds, function(err, salt) {
+                            bcrypt.genSalt(saltRounds, function (err, salt) {
                                 if (err) {
                                     throw err
                                 } else {
-                                    bcrypt.hash(req.body.newpassword, salt, function(err, hash) {
+                                    bcrypt.hash(req.body.newpassword, salt, function (err, hash) {
                                         if (err) {
                                             throw err
                                         } else {
@@ -308,7 +284,7 @@ exports.recoveryPassword = (req, res) => {
 
 
 
-            bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.genSalt(saltRounds, function (err, salt) {
                 if (err) {
                     throw err
                 } else {
@@ -316,7 +292,7 @@ exports.recoveryPassword = (req, res) => {
                         length: 8,
                         charset: 'alphabetic'
                     });
-                    bcrypt.hash(newpassword, salt, function(err, hash) {
+                    bcrypt.hash(newpassword, salt, function (err, hash) {
                         if (err) {
                             throw err
                         } else {
@@ -357,7 +333,7 @@ exports.recoveryPassword = (req, res) => {
                                             subject: mailData.subject,
                                             text: mailData.text + newpassword
                                         };
-                                        transporter.sendMail(mailOptions, function(error, info) {
+                                        transporter.sendMail(mailOptions, function (error, info) {
                                             if (error) {
                                                 res.status(400); //error
                                             } else {
